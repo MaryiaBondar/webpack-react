6b3e3b3e7c43ff623478bc09ee48b8587b66ab4d
MaryiaBondar <69987039+MaryiaBondar@users.noreply.github.com>
2020-12-03 21:21:22 +0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.scss'
+
+const App = () => {
+    console.log('Hello world')
+    return (
+        <div className='header-block error'>
+            Hello React!!!
+        </div>
+    )
+}
+
+export default App
